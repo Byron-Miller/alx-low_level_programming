a6bf0167777d15b3408306beb94515ae6533c109
Byron-Miller <bymiodh@gmail.com>
2022-06-30 17:44:48 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+*main - prints the string in write function
+*Description: using the main function this program prints
+*the string "and that piece of art is useful"
+*"- Dora Korpar, 2021-08-26",followed by a new line,
+*to the standard error.
+*Return: 1
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar,
+	2015-10-19\n", 59);
+	return (1);
+}
+
