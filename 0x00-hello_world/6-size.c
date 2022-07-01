b341297afa618e91ddc66ce550ab8c43aec10424
Byron-Miller <bymiodh@gmail.com>
2022-06-30 17:00:02 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,8 @@
 /**
 *main - print the string in the printf function
 *
-*Description:using the main function
-*this program prints the size of various types 
-*on the computer it is compiled and run on
+*Description: using the main function this program prints
+*the size of various types on the computer it is compiled and run on
 *Return: 0
 */
 int main(void)
@@ -15,6 +14,7 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
