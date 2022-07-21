d7c17d8768b180411118f668f965e3dcbd388a19
Byron-Miller <bymiodh@gmail.com>
2022-07-21 13:36:47 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,10 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int palindromeChecker(char *str, int len, int i);
+char *wildcard(char *s3, char *s4);
+int wildcmp(char *s1, char *s2);
 
 #endif
 
