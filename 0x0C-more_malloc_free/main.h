4a2ef4792957f581d68ba9d3d7c7c845e4bf00b4
Byron-Miller <bymiodh@gmail.com>
2022-07-28 13:46:12 -0700

    c-more mallocs

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,33 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: MAIN.h
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libmain.a.
+ */
+ 
+ int _putchar(char c);
+ void *malloc_checked(unsigned int b);
+ char *string_nconcat(char *s1, char *s2, unsigned int n);
+ void *_calloc(unsigned int nmemb, unsigned int size);
+ int *array_range(int min, int max);
+ 
+ #endif
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+ 
+
+ 
